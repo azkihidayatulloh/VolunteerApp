2896846a05d4547e92e7d81f0d8f271dec54216b
azkihidayatulloh <azkihidayatulloh@gmail.com>
2018-10-12 15:00:15 +0700

    Add some layouts and made some changes

---
+++ app/src/main/java/com/dicoding/millatip/volunteerapp/fragment/logout_alert.java
@@ -0,0 +1,30 @@
+package com.dicoding.millatip.volunteerapp.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dicoding.millatip.volunteerapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class logout_alert extends Fragment {
+
+
+    public logout_alert() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.logout_alert, container, false);
+    }
+
+}
