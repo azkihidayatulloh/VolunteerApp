074dbb45af363b8b5f8dfc1e90a56a285424bd0b
millatip <pratiwimillati@gmail.com>
2018-10-17 08:03:27 +0700

    ui revision

--- app/src/main/java/com/dicoding/millatip/volunteerapp/MainActivity.java
+++ app/src/main/java/com/dicoding/millatip/volunteerapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.dicoding.millatip.volunteerapp;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,6 +14,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.dicoding.millatip.volunteerapp.fragment.MyContributionsFragment;
+import com.dicoding.millatip.volunteerapp.fragment.TunaNetraFragment;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -20,23 +24,23 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         DrawerLayout drawer =
-                (DrawerLayout) findViewById(R.id.drawer_layout);
+                findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -47,21 +51,35 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+
         int id = item.getItemId();
+        Fragment fragment = null;
+        switch (id) {
+            case R.id.nav_home:
 
-        if (id == R.id.nav_home) {
-            // Handle the camera action
-        } else if (id == R.id.nav_tuna_netra) {
+                break;
+            case R.id.nav_tuna_netra:
+                fragment = new TunaNetraFragment();
+                break;
+            case R.id.nav_tuna_rungu:
 
-        } else if (id == R.id.nav_tuna_rungu) {
-
-        } else if (id == R.id.nav_my_contributions) {
-
+                break;
+            case R.id.nav_my_contributions:
+                fragment = new MyContributionsFragment();
+                break;
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (fragment!= null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.content_main, fragment)
+                    .commit();
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 }
+
+
---
+++ app/src/main/java/com/dicoding/millatip/volunteerapp/fragment/TunaNetraFragment.java
@@ -0,0 +1,30 @@
+package com.dicoding.millatip.volunteerapp.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dicoding.millatip.volunteerapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TunaNetraFragment extends Fragment {
+
+
+    public TunaNetraFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tuna_netra, container, false);
+    }
+
+}
